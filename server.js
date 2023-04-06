6d3a587397e7bfc98347491095ab24c4652b4a31
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-06 16:24:54 -0400

    set up the dependencies and import the necessary packages

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 // set up the dependencies. Add in the port, express, routes and listener
 require("dotend").config()
+
+// Pull the PORT from .env 
+const {PORT} = process.env
+
+// get PORT from .env 
+const express = require("express")
+
+// create an application object 
+const app = express()
+
+// import cors 
+const cors = require('cors')
