112074d56ae21e1dc5c3a1fece216be86d436183
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-06 16:25:55 -0400

    added in the middleware to server.js

--- server.js
+++ server.js
@@ -12,3 +12,13 @@ const app = express()
 
 // import cors 
 const cors = require('cors')
+
+
+
+// Middleware
+app.use(cors()) // this will prevent the cors errors and allow us to open access to all origins. An origin can be anything like localhost, heroku, etc
+app.use(express.urlencoded({extended:true})) // allow us to get req.body
+app.use(express.json()) // parse json
+
+
+
