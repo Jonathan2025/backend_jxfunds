01e4ce87a94e00db5bc1a575f37eca9e7c0e50ac
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-06 18:24:40 -0400

    created the fund schema model in fund.js

---
+++ models/Fund.js
@@ -0,0 +1,20 @@
+// now we will create a funds model 
+const mongoose = require("mongoose")
+
+// this is the schema and the attributes we want to show on the page 
+const FundSchema = new mongoose.Schema({
+    name: String,
+    company: String,
+    symbol: String, 
+    recommendation: String, 
+    date: Date,
+    timezone: String,
+    price: Number, 
+    dividends: Number
+})
+
+
+// now we need to create a model from our schema 
+const Fund = mongoose.model("Fund", FundSchema)
+
+module.exports = Fund
