7abf16249850733b283a54d11729ee5a45e19f4f
John Frick <johnfrickr@gmail.com>
2023-04-06 19:30:10 -0500

    added more routes

--- controllers/fundCtrls.js
+++ controllers/fundCtrls.js
@@ -6,7 +6,25 @@ const getFund = (req,res) =>{
     res.send('This is getFund.')
 }
 
+// This will let us create a new fund
+const createFund = (req,res) =>{
+    res.send('this is createFund')
+}
+
+// This will let us update a fund
+const updateFund = (req, res) =>{
+    res.send('this is updateFund')
+}
+
+// this will let us delete the fund
+const deleteFund = (req,res) => {
+    res.send('this is deleteFund')
+}
+
 // this will export our routes/controllers
 module.exports = {
-    getFund
+    getFund,
+    createFund,
+    updateFund,
+    deleteFund
 }
