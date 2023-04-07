afd8bf41c9aaeea51fcdbc14adfdc018faf32346
John Frick <johnfrickr@gmail.com>
2023-04-06 19:42:17 -0500

    added more routes to fundroutes.js

--- routes/fundRoutes.js
+++ routes/fundRoutes.js
@@ -3,6 +3,8 @@ const {fundCtrl} = require('../controllers')
 
 //ROUTES - METHODS
 router.get('/', fundCtrl.getFund)
-
+router.post('/',fundCtrl.createFund) 
+router.put('/:id', fundCtrl.updateFund)
+router.delete('/:id', fundCtrl.deleteFund)
 
 module.exports = router;
