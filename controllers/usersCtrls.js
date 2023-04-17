e293933ff96920b0bc102d87bd8f94ca14d9c2e8
John Frick <johnfrickr@gmail.com>
2023-04-16 20:03:46 -0500

    swapped jwt for session, and can see the session cookie created

--- controllers/usersCtrls.js
+++ controllers/usersCtrls.js
@@ -2,15 +2,17 @@
 const db = require('../models')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
 const { User } = require('../models')
 console.log(db)
 
 // now that we've required jsonwebtoken we will need a function to create the token, passing in the _id of the user as an arguement
-const createToken = (_id) => {
-    // below we are using the object ID, and the SECRET to create the token
-    // then we set it to expire in 1 day
-    return jwt.sign({_id},process.env.SECRET, {expiresIn: '1d'})
-}
+// const createToken = (_id) => {
+//     // below we are using the object ID, and the SECRET to create the token
+//     // then we set it to expire in 1 day
+//     return jwt.sign({_id: _id},process.env.SECRET, {expiresIn: '1d'})
+// }
 
 const getUser = (req, res) => {
     // res.send('This is getFund.')
@@ -30,11 +32,11 @@ const signInUser = async (req, res) =>{
 
     try {
         const user = await User.signIn(username, password)
-
+        req.session.user = result
         // we will create a token after they're saved in the database
-        const token = createToken(user._id)
-
-        res.status(200).json({ username, token })
+        // const token = createToken(user._id)
+        console.log(req.session.user)
+        res.status(200).json({ username, password })
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
@@ -49,9 +51,9 @@ const createUser = async (req, res) => {
         const user = await User.signup(username, password)
 
         // we will create a token after they're saved in the database
-        const token = createToken(user._id)
+        // const token = createToken(user._id)
 
-        res.status(200).json({ username, token })
+        res.status(200).json({ username, password })
         
     } catch (error) {
         res.status(400).json({ error: error.message })
@@ -64,17 +66,3 @@ module.exports = {
     createUser,
     signInUser
 }
-
-// Historic code for reference
-// This will let us create a new User
-// const createUser = (req,res) =>{
-//     // res.send('this is createFund')
-//     db.User.create(req.body)
-//     .then((createdUser)=>{
-//         if(!createdUser){
-//             res.status(400).json({message:'cannot create User'})
-//         } else {
-//             res.status(201).json({data: createdUser, message:'User created'})
-//         }
-//     })
-// }
